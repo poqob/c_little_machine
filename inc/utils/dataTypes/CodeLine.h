983947260cd7db4bcfd60c48eb370f8052fe9782
Mustafa <mmermmus123@gmail.com>
2024-05-04 16:34:15 +0300

    codeline able to store word series.

--- inc/utils/dataTypes/CodeLine.h
+++ inc/utils/dataTypes/CodeLine.h
@@ -1,11 +1,15 @@
 #if !defined(CODELINE)
 #define CODELINE
 #define MAXFIELDS 1000
+#include <stdlib.h>
 typedef struct CodeLine CodeLine;
+// each code line have to start with a valid operator word. [look for operatorsEnum.h]
 struct CodeLine
 {
-    char *words[MAXFIELDS];
+    char **words;
     int wordCount;
 };
+CodeLine *newCodeLine();
+CodeLine *newCodeLineWithParameters(char **words, int wordCount);
 
 #endif // CODELINE
--- src/utils/dataTypes/CodeLine.c
+++ src/utils/dataTypes/CodeLine.c
@@ -1,2 +1,16 @@
 #include "../../../inc/utils/dataTypes/CodeLine.h"
 // dump
+CodeLine *newCodeLine()
+{
+    CodeLine *this = (CodeLine *)malloc(sizeof(CodeLine));
+    this->wordCount = 0;
+    return this;
+}
+
+CodeLine *newCodeLineWithParameters(char **words, int wordCount)
+{
+    CodeLine *this = (CodeLine *)malloc(sizeof(CodeLine));
+    this->wordCount = wordCount;
+    this->words = words;
+    return this;
+}
